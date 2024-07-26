23126ffa6e3db4a8792d77294fa6c426eb99ed3f
HugoAparecido <hugoapga626@gmail.com>
2024-07-26 09:19:57 -0300

    classe login

--- componentes/classes/login_class.php
+++ componentes/classes/login_class.php
@@ -6,12 +6,12 @@ class Usuario
      * Identificador único do Usuário
      * @var integer
      */
-    public $id_usuario;
+    private $id_usuario;
     /**
      * Nome do Usuário
      * @var string
      */
-    public $nome_usuario;
+    private $nome_usuario;
     /**
      * Email do Usuário
      * @var string
@@ -43,8 +43,37 @@ class Usuario
      * @param string $senha
      * @return array
      */
+    public function __construct($nome = null, $senha = null, $email = null, $jogador = null, $id_jogador = null, $treinador = null){
+    $this->$id_usuario = $nome;
+    $this->$nome_usuario = $senha;
+    $this->$email_usuario = $email;
+    $this->$senha_usuario = $jogador;
+    $this->$jogador = $id_jogador;
+    $this->$id_jogador = $id_jogador;
+    $this->$treinador = $treinador;
+    }
     public static function Logar($email, $senha)
     {
         return (new Database('usuario'))->select("email_usuario = '$email' AND senha_usuario = '$senha'")->fetchAll(PDO::FETCH_CLASS, self::class);
+    }
+    public static function Deslogar($redirecionar) {
+        session_destroy();
+        header("Location: $redirecionar");
+    }
+    public static function Cadastrar(){
+        if($this->treinador){
+        //INSERIR O USUÁRIO NO BANCO
+        $obDatabase = new Database('usuario');
+        $this->id = $obDatabase->insert([
+            'nome' => $this->nome,
+            'nome_usuario' => $this->nome_usuario,
+            'email_usuario' => $this->email_usuario,
+            'senha_usuario' => $this->senha_usuario,
+            'jogador' => $this->jogador,
+            'id_jogador' => $this->id_jogador,
+            'treinador' => $this->treinador
+        ]);
+        //RETORNAR SUCESSO
+        return true;}
     }
 }
