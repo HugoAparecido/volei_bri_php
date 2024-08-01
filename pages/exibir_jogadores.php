9585de7b595d9c73ac5eec56198f02733f346633
AaronKKFX <aaron.k@aluno.ifsp.edu.br>
2024-07-31 21:17:28 -0300

    formatação card

--- pages/exibir_jogadores.php
+++ pages/exibir_jogadores.php
@@ -53,3 +53,28 @@ if (isset($_SESSION['id_usuario'])) {
         </tbody>
     </table>
     </div>
+<div>
+<div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Nome Jogador: </h5>
+        <p class="card-title">Posição</p>
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Fundamento
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Passe</a></li>
+                <li><a class="dropdown-item" href="#">Ataque</a></li>
+                <li><a class="dropdown-item" href="#">Defesa...</a></li>
+            </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
