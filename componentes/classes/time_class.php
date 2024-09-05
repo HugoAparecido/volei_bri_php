def59d95f5f1b867d128378dab95f15a1e8e4e8d
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2024-09-05 15:28:12 -0300

    Começando a classe times

--- componentes/classes/time_class.php
+++ componentes/classes/time_class.php
@@ -6,27 +6,32 @@ class Time
      * Identificador único do time
      * @var integer
      */
-    public $id;
+    public $id_time;
     /**
-     * Nome do Jogador
+     * Nome do Time
      * @var string
      */
-    public $nome;
+    public $nome_time;
     /**
-     * Jogadores que estão no time
-     * @var array
-     */
-    public $jogadoresNoTime;
-    /**
-     * Nome do Jogador
+     * Data de criação do time
      * @var string
      */
-    public $dataCriacao;
+    public $data_hora_criacao;
     /**
      * Sexo do Time
      * @var string(M/F/MIS)
      */
-    public $sexo;
+    public $sexo_time;
+    /**
+     * Usuário que cadastrou o time
+     * @var int
+     */
+    public $id_usuario;
+    /**
+     * Instituição do time
+     * @var int
+     */
+    public $id_istituicao;
     /**
      * Método responsável por Cadastrar um novo time no banco
      * @return boolean
@@ -34,63 +39,58 @@ class Time
     public function Cadastrar()
     {
         //DEFINIR A DATA
-        $this->dataCriacao = date('Y-m-d H:i:s');
-        //INSERIR O JOGADOR NO BANCO
+        $this->data_hora_criacao = date('Y-m-d H:i:s');
+        //INSERIR O TIME NO BANCO
         $obDatabase = new Database('time');
-        $this->id = $obDatabase->insert([
-            'nome' => $this->nome,
-            'posicao' => $this->posicao,
-            'num_camisa' => $this->num_camisa,
-            'apelido' => $this->apelido,
-            'altura' => $this->altura,
-            'peso' => $this->peso,
-            'sexo' => $this->sexo
+        $this->id_time = $obDatabase->insert([
+            'nome_time' => $this->nome_time,
+            'data_hora_criacao' => $this->data_hora_criacao,
+            'sexo_time' => $this->sexo_time,
+            'id_usuario' => $this->id_usuario,
+            'id_istituicao' => $this->id_istituicao
         ]);
         //RETORNAR SUCESSO
         return true;
     }
     /**
-     * Método responsável por atualizar os jogadores no banco
+     * Método responsável por atualizar os times no banco
      * @return boolean
      */
     public function Atualizar()
     {
-        return (new Database('jogador'))->update('id = ' . $this->id, [
-            'nome' => $this->nome,
-            'posicao' => $this->posicao,
-            'num_camisa' => $this->num_camisa,
-            'apelido' => $this->apelido,
-            'altura' => $this->altura,
-            'peso' => $this->peso,
-            'sexo' => $this->sexo
+        return (new Database('time'))->update('id = ' . $this->id_time, [
+            'nome_time' => $this->nome_time,
+            'data_hora_criacao' => $this->data_hora_criacao,
+            'sexo_time' => $this->sexo_time,
+            'id_istituicao' => $this->id_istituicao
         ]);
     }
     /**
-     * Método responsável por excluir o jogador do banco
+     * Método responsável por excluir o time do banco
      * @return boolean
      */
     public function Excluir()
     {
-        return (new Database('jogador'))->delete('id = ' . $this->id);
+        return (new Database('time'))->delete('id = ' . $this->id_time);
     }
     /**
-     * Método responsável por obter os jogadores do banco de dados
+     * Método responsável por obter os times do banco de dados
      * @param string $where
      * @param string $order
      * @param string $limit
      * @return array
      */
-    public static function getJogadores($where = null, $order = null, $limit = null)
+    public static function GetTimes($where = null, $order = null, $limit = null)
     {
-        return (new Database('jogador'))->select($where, $order, $limit)->fetchAll(PDO::FETCH_CLASS, self::class);
+        return (new Database('time'))->select($where, $order, $limit)->fetchAll(PDO::FETCH_CLASS, self::class);
     }
     /**
-     * Método responsável por buscar um jogador com base em seu ID
-     * @param integer $id
-     * @return jogador
+     * Método responsável por buscar um time com base em seu ID
+     * @param integer $id_time
+     * @return time
      */
-    public static function getJogador($id)
+    public static function GetTime($id_time)
     {
-        return (new Database('jogador'))->select('id = ' . $id)->fetchObject(self::class);
+        return (new Database('time'))->select('id = ' . $id_time)->fetchObject(self::class);
     }
 }
