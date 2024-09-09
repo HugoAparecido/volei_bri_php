6d0cb15f534804ce6c476b37a459dc5b1b46692f
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2024-09-09 12:10:08 -0300

    Update protect.php

--- componentes/protect.php
+++ componentes/protect.php
@@ -2,8 +2,3 @@
 if (!isset($_SESSION)) {
     session_start();
 }
-if (!isset($_SESSION['id_usuario'])) {
-?><script>
-        window.location.href = "./login.php"
-    </script><?php
-            }
