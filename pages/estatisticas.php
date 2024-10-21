18f0b3a203a7ac5a50b6d125e94fae3937e937f9
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2024-10-21 13:45:52 -0300

    Update estatisticas.php
    Modificando o visual

--- pages/estatisticas.php
+++ pages/estatisticas.php
@@ -41,58 +41,94 @@ include '../componentes/header.php';
             <a href="../componentes/logout.php">Sair</a>
         </button>
     </div>
+
+    <!-- Verificação se o parâmetro 'id_time' não está presente na URL -->
     <?php if (!isset($_GET['id_time'])) { ?>
+
+        <!-- Formulário exibido quando o time não é selecionado, permitindo escolher um time -->
         <form action="./estatisticas.php" method="get" class="mt-5">
             <select name="id_time" id="id_time">
                 <?php
+                // Função que popula o 'select' com os times disponíveis
                 Componentes::InputTimes();
                 ?>
             </select>
             <button type="submit">Mostrar</button>
         </form>
+
+        <!-- Caso o 'id_time' já esteja presente na URL -->
     <?php } else { ?>
+
+        <!-- Exibe o título "Estatísticas" -->
         <h1>Estatísticas</h1>
         <div class="card">
             <?php
+            // Obtenção de jogadores de um time específico
             $objetos = JogadorTime::getJogadoresTime('id_time = ' . intval($_GET['id_time']), null, null, 'id_jogador_time, posicao_jogador');
+
+            // Cria um array de IDs de jogadores
             $ids = [];
             foreach ($objetos as $objeto) {
                 array_push($ids, $objeto->GetID());
             }
-            echo "<pre>";
-            print_r($objetos);
-            print_r($ids);
-            echo "</pre>";
+            // Obtenção das estatísticas de defesas para o time selecionado
             $estatisticas = JogadorTime::GetEstatiticasSomaGeralDefesas(intval($_GET['id_time']));
             $estatisticas = $estatisticas[0];
+
+            // Envia o valor das defesas para o JavaScript
             echo '<script>var defesas = ' . $estatisticas->GetDefesas() . ';</script>';
+
+            // Obtenção das estatísticas de passes para o líbero no time
             $estatisticas = JogadorTime::GetEstatiticasSomaGeralPasses($ids, 'libero_no_time');
             $estatisticas = $estatisticas[0];
+
+            // Envia o valor dos passes do líbero para o JavaScript
             echo '<script>var passesLibero = ' . $estatisticas->GetPasses() . ';</script>';
+
+            // Obtenção das estatísticas de passes para outras posições no time
             $estatisticas = JogadorTime::GetEstatiticasSomaGeralPasses($ids, 'outras_posicoes_no_time');
             $estatisticas = $estatisticas[0];
+
+            // Envia o valor dos passes das outras posições para o JavaScript
             echo '<script>var passesOutrasPosicoes = ' . $estatisticas->GetPasses() . ';</script>';
             ?>
-            <div style="width: 500px;">
-                <h2>Passes</h2>
-                <div id="grafico_passe_libero_local">
-                    <h3>Líbero</h3>
+
+            <!-- Seção de gráficos para exibir as estatísticas de passes e defesas -->
+            <div class="card">
+                <div class="card-header text-center">
+                    <h2>Passes</h2>
                 </div>
-                <div id="grafico_passe_outras_local">
-                    <h3>Outras Posições</h3>
-                </div>
-                <div id="grafico_passe_total_local">
-                    <h3>Outras Posições</h3>
+                <div class="d-flex flex-row">
+                    <div class="text-center" id="grafico_passe_libero_local" style="width: 500px;">
+                        <h3>Líbero</h3>
+                    </div>
+                    <div class="text-center" id="grafico_passe_outras_local" style="width: 500px;">
+                        <h3>Outras Posições</h3>
+                    </div>
+                    <div class="text-center" id="grafico_passe_total_local" style="width: 500px;">
+                        <h3>Total Passes Time</h3>
+                    </div>
                 </div>
             </div>
-            <div id="grafico_defesa_local" style="width: 500px;">
-                <h2>Defesas</h2>
+
+            <!-- Gráfico para exibir as defesas -->
+            <div class="card">
+                <div class="card-header text-center">
+                    <h2>Defesas</h2>
+                </div>
+                <div id="grafico_defesa_local">
+                </div>
             </div>
         </div>
+
+        <!-- Fim da condição -->
     <?php } ?>
 </main>
+
+<!-- Inclusão de bibliotecas de JavaScript para criação de gráficos -->
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script type="module" src="../js/estatisticas_time.js"></script>
+
 <?php
 // Inclui o rodapé da página, contendo scripts e informações finais.
 include '../componentes/footer.php';
